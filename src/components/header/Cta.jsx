b37e3f38efece716328bf4d35269c39c6e22da4d
Venkata Sai Ram Reddy <venkatasairamreddy0404@gmail.com>
2024-03-16 19:05:47 +0530

    added Timeline and added toastify

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -4,7 +4,7 @@ import CV from '../assests/document/Resume.pdf'
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href="https://flowcv.com/resume/hks1hej1ub" download className='btn'>Download CV</a>
         <a href="#contact"className='btn btn-primary' >Let's Talk</a>
     </div>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header_container">
         <h5>Hello I'm</h5>
-        <h1>Venkata Sai Ram</h1>
+        <h1>Venkata Sai Ram Reddy</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <Cta/>
         <Header_Social/>
